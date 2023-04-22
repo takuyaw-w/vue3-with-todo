76321144bc6cf55c37e9d99ea278c7c577e9a3e1
takuyaw-w <takuya.jamtb@gmail.com>
2023-04-23 02:08:31 +0900

    fix: styling

--- src/views/CreateTodo.vue
+++ src/views/CreateTodo.vue
@@ -34,16 +34,63 @@ function addTodo() {
     </ul>
   </nav>
   <form @submit.prevent="addTodo">
-    <div>
+    <div class="input-box">
       <label for="title">Title</label>
       <input type="text" id="title" v-model="newTodo.title" />
     </div>
-    <div>
+    <div class="input-box">
       <label for="description">Description</label>
       <textarea id="description" v-model="newTodo.description"></textarea>
     </div>
-    <div>
+    <div class="action">
       <button type="submit">Todoを追加する</button>
     </div>
   </form>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+
+nav > ul {
+  display: flex;
+  justify-content: flex-start;
+  margin-bottom: 1rem;
+}
+input,
+button,
+textarea {
+  border: 1px solid #ccc;
+}
+input {
+  padding: 5px;
+}
+textarea {
+  padding: 5px;
+}
+.input-box {
+  display: flex;
+}
+.input-box label {
+  flex-basis: 150px;
+}
+.input-box input,
+.input-box textarea {
+  flex-grow: 1;
+}
+.input-box + .input-box {
+  margin-top: 1rem;
+}
+.action {
+  margin-top: 1rem;
+}
+.action button {
+  padding: 15px 10px;
+  background-color: ;
+  border-radius: 5px;
+  width: 100%;
+  background-color: indigo;
+  color: #fff;
+}
+</style>
